ac9f676f8784cb935ffdad4eeb3a72abb2d7941a
genki_sanyu <baseballboy1025@gmail.com>
2021-05-27 16:50:55 +0900

    20210527

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 <body>
     <form action="" method="POST">
         <div class="form form-1">
-            <div class="row">
-                <p class="form__item form__item-1 col-12 m-0 p-0">依頼タイトル</p>
+            <div class="row form__item form__item-1">
+                <p class="col-12 m-0 p-0">依頼タイトル</p>
                 <input class="form__input form__input-1 col-12 m-0 p-0" type="text" placeholder="例：アウトドア用品店のホームページ制作">
             </div>
         </div>
         <div class="form form-2">
-            <div class="row">
-                <p class="form__item form__item-2 col-3">依頼主の業種</p>
-                <select name="industry" id="">
+            <div class="row form__item form__item-2">
+                <p class="col-12 m-0 p-0">依頼主の業種</p>
+                <select class="form__select form__select-2 col-12 m-0 p-0" name="industry" id="">
                     <option value="none">- 依頼主の業種を選択 -</option>
                     <option value="1">IT・通信・インターネット</option>
                     <option value="2">マスコミ・メディア</option>
@@ -71,24 +71,48 @@
                     <option value="49">宗教</option>
                     <option value="50">その他</option>
                 </select>
+
             </div>
         </div>
         <div class="form form-3">
             <div class="row">
-                <p class="form__item form__item- col-2"></p>
-                <input class="form__input form__input- col-5" type="" placeholder="">
+                <p class="form__item form__item-3 col-12 m-0 p-0">サイトの種類</p>
+                <select class="form__select form__select-2 col-12 m-0 p-0" name="industry" id="">
+                    <option value="none">- サイトの種類を選択 -</option>
+                    <option value="1">企業サイト</option>
+                    <option value="2">店舗サイト</option>
+                    <option value="3">通販サイト・ネットショップ</option>
+                    <option value="4">ランディングページ</option>
+                    <option value="5">プロモーションサイト</option>
+                    <option value="6">採用・求人サイト</option>
+                    <option value="7">メディアサイト</option>
+                    <option value="8">会員サイト</option>
+                    <option value="9">掲示板サイト</option>
+                    <option value="10">口コミサイト</option>
+                    <option value="11">ブログ</option>
+                    <option value="12">アフィリエイトサイト</option>
+                    <option value="13">多言語サイト</option>
+                    <option value="14">その他</option>
+                </select>
             </div>
         </div>
         <div class="form form-4">
             <div class="row">
-                <p class="form__item form__item- col-2"></p>
-                <input class="form__input form__input- col-5" type="" placeholder="">
+                <p class="form__item form__item-4 col-12 m-0 p-0">ご依頼の目的・背景</p>
+                <textarea class="form__textarea form__textarea-4 col-12 m-0 p-0" name="goal" id="" cols="30" rows="4" placeholder="例：新しく女性向けの雑貨屋を立ち上げることになったため、店の魅力が伝わるようなホームページを開設したいと考えています。"></textarea>
             </div>
         </div>
         <div class="form form-5">
             <div class="row">
-                <p class="form__item form__item- col-2"></p>
-                <input class="form__input form__input- col-5" type="" placeholder="">
+                <p class="form__item form__item-5 col-12 m-0 p-0">期待する効果</p>
+                <div><input class="form__checkbox form__checkbox-1" id="form__checkbox-1" type="checkbox"><label class="form__label" for="form__checkbox-1">検索エンジンの上位に表示してアクセスを増やしたい</label></div>
+                <div><input class="form__checkbox form__checkbox-2" id="form__checkbox-2" type="checkbox"><label class="form__label" for="form__checkbox-2">商品、サービスの認知度を上げたい</label></div>
+                <div><input class="form__checkbox form__checkbox-3" id="form__checkbox-3" type="checkbox"><label class="form__label" for="form__checkbox-3">ネットショップの売り上げを伸ばしたい</label></div>
+                <div><input class="form__checkbox form__checkbox-4" id="form__checkbox-4" type="checkbox"><label class="form__label" for="form__checkbox-4">問い合わせを増やして新規リードを獲得したい</label></div>
+                <div><input class="form__checkbox form__checkbox-5" id="form__checkbox-5" type="checkbox"><label class="form__label" for="form__checkbox-5">スマートフォンに対応させたい</label></div>
+                <div><input class="form__checkbox form__checkbox-6" id="form__checkbox-6" type="checkbox"><label class="form__label" for="form__checkbox-6">サイトが古いのでリニューアルしたい</label></div>
+                <div><input class="form__checkbox form__checkbox-7" id="form__checkbox-7" type="checkbox"><label class="form__label" for="form__checkbox-7">多言語対応で海外展開をしたい</label></div>
+                <textarea class="form__textarea form__textarea-5 col-12 m-0 p-0" name="effect" id="" cols="30" rows="10" placeholder="その他の期待する効果がある場合は入力してください"></textarea>
             </div>
         </div>
         <div class="form form-6">
